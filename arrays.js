a27a9a558160d6cfe74be261ff99753afb2b4b20
ineffable-coder-0435 <gerren62@gmail.com>
2020-05-24 14:23:10 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,1 +1,7 @@
 var chocolateBars = ["snickers","hundred grand","kitkat","skittles"];
+
+destructivelyAddElementToBeginningOfArray([1,2,3],4){
+  var beginning = [1,2,3]
+  beginning.unshift(4)
+}
+
